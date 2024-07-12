cae0faa8a185509edcdc19a993a68ce9f45b2dc9
AmitStredz <ponnan2112@gmail.com>
2024-07-12 17:41:36 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="stredz">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
